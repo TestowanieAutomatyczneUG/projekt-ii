024cca84354fa39578c6359c46b839cae65a362b
Mateusz Mackowiak <darkstaron123@gmail.com>
2022-01-21 01:02:22 +0100

    test_parameterizedOrder setup

---
+++ tests/test_parameterizedOrder.py
@@ -0,0 +1,27 @@
+import unittest
+from assertpy import *
+from src.order import *
+from parameterized import *
+from unittest.mock import *
+
+@parameterized_class(('wrongValueInt', 'wrongValueFloat'), [
+    (True, False),
+    (None, None),
+    (-8.0, 9),
+    ("", ""),
+    ("Cosiek", 0),
+    ([9, 3, 6], [6, 3, 9]),
+    (9.0, "Bobert"),
+    ({'firstName': 8, 'lastName': 2, 'thirdName': 3}, {'firstName': 90, 'lastName': 5, 'thirdName': 4})
+])
+class TestsParametrizedOrder(unittest.TestCase):
+
+    def setUp(self):
+        def mock_get_orders():
+            return [(709, 177)]
+        Order.ordery = []
+        self.order = Order(mock_get_orders()[0][0], mock_get_orders()[0][1])
+
+
+    def tearDown(self):
+        del self.order
