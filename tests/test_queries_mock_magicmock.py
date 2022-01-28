da3737c1ed713bb575483f6c44399992e03dc96b
Mateusz Mackowiak <darkstaron123@gmail.com>
2022-01-28 22:25:19 +0100

    test_mockClass_get_items_from_orders_by_id

--- tests/test_queries_mock_magicmock.py
+++ tests/test_queries_mock_magicmock.py
@@ -24,3 +24,6 @@ class TestsMock(unittest.TestCase):
     def test_mockClass_get_orders_from_item_by_id(self):
         self.mockClass.get_orders_from_item_by_id = MagicMock(return_value=[(1,2),(1,3)])
         assert_that(self.mockClass.get_orders_from_item_by_id(1)).is_equal_to([(1,2),(1,3)])
+    def test_mockClass_get_items_from_orders_by_id(self):
+        self.mockClass.test_mockClass_get_items_from_orders_by_id = MagicMock(return_value=[(1,2)])
+        assert_that(self.mockClass.test_mockClass_get_items_from_orders_by_id(2)).is_equal_to([(1,2)])
